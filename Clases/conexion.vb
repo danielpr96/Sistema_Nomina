47fa6cde86e19dcfbd6cc078d20ae984fece76ac
Alejandro Sanchez <charlie_144@msn.com>
2017-07-17 22:32:57 -0600

    se agregaron las clases conexion y procedimientos

---
+++ Clases/conexion.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Data.SqlClient
+
+Public Class conexion
+    Public cnn As SqlConnection
+
+    Sub abrirConexion()
+        Try
+            cnn = New SqlConnection("Data Source=DANIELPR96\SQLEXPRESS; Initial Catalog=nomias; User Id=dportillo; Password=123456")
+            cnn.Open()
+        Catch ex As Exception
+            MsgBox("No conecto" + ex.ToString)
+        End Try
+    End Sub
+
+    Sub cerrarConexion()
+        Try
+            With (cnn)
+                .Close()
+                .Dispose()
+            End With
+            cnn = Nothing
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
---
+++ Procedimientos/procedimientos.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Data.SqlClient
+Public Class procedimientos
+
+    Public Sub AltaNomina(ByVal fechaComprobante As DateTime, ByVal formaPago As String, ByVal noCertificado As String, ByVal certificado As String, ByVal subTotalComprobante As Decimal,
+                          ByVal descuentoComprobante As Decimal, ByVal moneda As String, ByVal totalComprobante As Decimal)
+        Try
+            Dim conexion As New conexion
+            conexion.abrirConexion()
+            Dim cmd As New SqlCommand
+            cmd.CommandText = "[AltaNomina]"
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Connection = conexion.cnn
+            cmd.Parameters.AddWithValue("@fechaComprobante", fechaComprobante)
+            cmd.Parameters.AddWithValue("@formaPago", formaPago)
+            cmd.Parameters.AddWithValue("@noCertificado", noCertificado)
+            cmd.Parameters.AddWithValue("@certificado", certificado)
+            cmd.Parameters.AddWithValue("@subTotalComprobante", subTotalComprobante)
+            cmd.Parameters.AddWithValue("@descuentoComprobante", descuentoComprobante)
+            cmd.Parameters.AddWithValue("@moneda", moneda)
+            cmd.Parameters.AddWithValue("@totalComprobante", totalComprobante)
+            cmd.ExecuteNonQuery()
+            conexion.cerrarConexion()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+End Class
